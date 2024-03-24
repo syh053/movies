d1deb4b7b6647862e6960fa8063aa17dba3c41e4
syh053 <syh053@gmail.com>
2024-03-24 15:15:07 +0800

    feat : add detail page & add search function to listing

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require("express")
-const { engine } = require("express-handlebars")
 const app = express()
+
+// 花括號寫法
+const { engine } = require('express-handlebars')
+
+// 如果不用花括號，就必須用下面寫法
+// const exphbs  = require('express-handlebars')
+// const engine = exphbs.engine
+
 const port = 3000
 const movies = require("./public/jsons/movies.json").results
 const image_url = "https://movie-list.alphacamp.io/posters/"
 
 
-app.engine('.hbs', engine({ extname: '.hbs' }));
-app.set('view engine', '.hbs');
-app.set('views', './views');
+app.engine('.hbs', engine({ extname: '.hbs' }))
+app.set('view engine', '.hbs')
+app.set('views', './views')
 
 
 app.use(express.static("public"))
@@ -20,13 +27,22 @@ app.get("/", (req, res) => {
 
 
 app.get("/movies", (req, res) => {
-    res.render("index", { movies, image_url })
+    const keyword = req.query.search
+    const filter_movies = keyword ? movies.filter(movie => Object.values(movie).some(property => {
+        if (typeof property === "string") {
+            return property.toLowerCase().includes(keyword.toLowerCase())
+        }
+    })  
+    ) : movies
+
+    res.render("index", { movies: filter_movies, image_url, keyword })
 })
 
 
 app.get("/movie/:id", (req, res) => {
-    const id = req.params.id
-    res.send(`read movie : ${id}`)
+    const id = Number(req.params.id)
+    const movie = movies.find((movie) => movie.id === id)
+    res.render("detail", { movie, image_url })
 })
 
 
